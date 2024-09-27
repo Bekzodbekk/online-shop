229bf42674c575e64edf91ae0803f8bfd4df7411
Bekzodbekk <bekzodnematov709@gmail.com>
2024-09-27 17:57:10 +0500

    first

--- src/Components/Products/Pages/AddCountProduct/AddCountProduct.jsx
+++ src/Components/Products/Pages/AddCountProduct/AddCountProduct.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./addProductCount.scss"
 
 const AddCountProduct = ({ isOpen, toggleWindow }) => { // Prop orqali oynani boshqarish
     const handleOutsideClick = (e) => {
